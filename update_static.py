27749441f062d2b71e614c689025ab16932aa79d
Srecko Skocilic <srecko.skocilic@gmail.com>
2026-02-13 18:42:28 +0100

    redirect stdout to log file

--- update_static.py
+++ update_static.py
@@ -1,4 +1,5 @@
 import json
+import sys
 import time
 from datetime import timedelta, datetime
 
@@ -8,63 +9,45 @@ from nba_api.stats.library.data import players
 from common.games import get_games_list
 
 
-def dump_players_from_static_file():
-    for player_entry in players:
-        player_entry.append(0)
-    with open("players.json", "w") as output_file:
-        json.dump(players, output_file, indent=4)
-
-
-def remove_active_field():
-    with open("players.json", "r") as players_file:
-        pl = json.load(players_file)
-    for sublist in pl:
-        del sublist[4]
-        del sublist[2]
-        del sublist[1]
-    with open("players_with_teamid.json", "w") as save_file:
-        json.dump(pl, save_file, indent=4)
-
-
 def update_players():
-    date_offset = 2
+    date_offset = 1
     now = datetime.now()
 
-    while date_offset > 0:
-        with open("players_with_teamid.json", "r") as file:
-            players_with_teamid = json.load(file)
+    log_file = open("trades.log", "w")
+    sys.stdout = log_file
 
-        changes = 0
-        changed_players = []
-        off_date = (now + timedelta(days=-date_offset)).strftime("%d-%m-%Y")
-        for game in get_games_list(date_offset):
-            try:
-                bs_stats = boxscoretraditionalv3.BoxScoreTraditionalV3(game_id=game)
-            except:
-                continue
+    with open("players_with_teamid.json", "r") as file:
+        players_with_teamid = json.load(file)
 
-            for player in bs_stats.player_stats.get_dict()["data"]:
-                p = next((x for x in players_with_teamid if x[0] == player[6]), None)
-                if p is not None:
-                    if player[12] == "":
-                        if p[2] != player[1]:
-                            p[2] = player[1]
-                            changes += 1
-                            changed_players.append(player)
-        with open("players_with_teamid.json", "w") as ffile:
-            json.dump(players_with_teamid, ffile, indent=4)
+    changes = 0
+    changed_players = []
+    off_date = (now + timedelta(days=-date_offset)).strftime("%d-%m-%Y")
+    for game in get_games_list(date_offset):
+        try:
+            bs_stats = boxscoretraditionalv3.BoxScoreTraditionalV3(game_id=game)
+        except Exception:
+            continue
 
-        if changes > 0:
-            print(f"Date: {off_date} Changes: {changes}")
-            for player in changed_players:
-                print(player)
-        else:
-            print(f"Date: {off_date}")
+        for player in bs_stats.player_stats.get_dict()["data"]:
+            p = next((x for x in players_with_teamid if x[0] == player[6]), None)
+            if p is not None:
+                if player[12] == "":
+                    if p[2] != player[1]:
+                        p[2] = player[1]
+                        changes += 1
+                        changed_players.append(player)
+    with open("players_with_teamid.json", "w") as ffile:
+        json.dump(players_with_teamid, ffile, indent=4)
 
-        date_offset -= 1
-        time.sleep(5)
+    if changes > 0:
+        print(f"Date: {off_date} Changes: {changes}")
+        for player in changed_players:
+            print(player)
+    else:
+        print(f"Date: {off_date}")
+
+    log_file.close()
 
 
 if __name__ == "__main__":
     update_players()
-    # remove_active_field()
